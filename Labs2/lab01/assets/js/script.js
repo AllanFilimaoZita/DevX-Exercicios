86ba57b38f3955d97e9bb4ec444ebbe9c389fa06
Allan Filimao Zita <allanfilimaozita@gmail.com>
2026-03-04 17:02:00 +0200

    Adicionando os exercicios da Lab2

---
+++ Labs2/lab01/assets/js/script.js
@@ -0,0 +1,10 @@
+// Selecionando todos os elementos usando querySelectorAll
+const elementos = document.querySelectorAll('.container, .card, p, button');
+
+// Mostrando no console o número de elementos
+console.log(elementos);
+
+// Selecionando o primeiro card
+const elemento = document.querySelector('p');
+
+elemento.innerText = "Produto 2"
