02ecccea5e4254fd3fe5500af46e85e80afabf11
Allan Filimao Zita <allanfilimaozita@gmail.com>
2026-03-17 00:33:39 +0200

    Adicionando Labs 3 da DevX

---
+++ Labs3/lab01/assets/js/script.js
@@ -0,0 +1,11 @@
+let aluno = {
+    nome: 'Allan',
+    idade: 19,
+    cidade: 'Matola C',
+    curso: 'Programação'
+}
+
+aluno.cidade = 'Cidade da Matola';
+aluno.experiencia = 'Júnior';
+
+console.log(aluno.nome)
